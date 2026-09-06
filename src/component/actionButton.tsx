cac3a06e8df47b8bd1eb9e35809ec0bdfd64346f
Muhamad Fathi Farhat <muh.fathifarhat18@gmail.com>
2026-09-06 17:27:42 +0700

    Update actionButton.tsx

--- src/component/actionButton.tsx
+++ src/component/actionButton.tsx
@@ -8,7 +8,7 @@ export default function ActionButtons() {
         href="/Muhamad_Fathi_Farhat_CV.pdf"
         target="_blank"
         rel="noopener noreferrer"
-        className="flex items-center justify-center gap-1.5 lg:gap-2 px-2 sm:px-3 py-2 lg:py-2.5 rounded-md border border-black/15 bg-[#e6e6e6] text-black text-[11px] lg:text-xs font-mono whitespace-nowrap cursor-pointer transition-all duration-200 hover:-translate-y-0.5 hover:shadow-md hover:bg-[#c9c9c9]">
+        className="flex items-center justify-center gap-1.5 lg:gap-2 px-2 sm:px-3 py-2 lg:py-2.5 rounded-md border border-black/65 bg-[#e6e6e6] text-black text-[11px] lg:text-xs font-mono whitespace-nowrap cursor-pointer transition-all duration-200 hover:-translate-y-0.5 hover:shadow-md hover:bg-[#c9c9c9]">
         <FileText size={14} className="shrink-0" />
         Download CV
       </a>
